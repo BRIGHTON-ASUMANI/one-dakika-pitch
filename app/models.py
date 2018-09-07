a3ffd2d5dd0d05e5ec9411c5360dedd15b727db4
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-09-07 15:08:47 +0300

    i aupdated my database and the auth views. The project is not currently displaying

--- app/models.py
+++ app/models.py
@@ -4,13 +4,20 @@ from flask_login import UserMixin
 from . import login_manager
 #creating database for class user
 
-class User(|UserMixin,db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
+
+
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
-    email = db.Column(db.String(255),unique = True,index = True)
-    pass_secure  = db.Column(db.String(255))
-    password_hash = db.Column(db.String(255))
+    email=db.Column(db.String(255),unique=True,index=True)
+    password_hash=db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
         @property
         def password(self):
             raise AttributeError('You cannot read the password attribute')
