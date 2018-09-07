90863dd481bb271572d0e00675852390f4bbd52d
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-09-07 13:56:54 +0300

    i enabled the pass secure in my column

--- app/models.py
+++ app/models.py
@@ -6,7 +6,10 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
-    comment = db.relationship("Comment", backref="user", lazy = "dynamic")
+    pass_secure = db.Column(db.String(255))
+
+
+    # comment = db.relationship("Comment", backref="user", lazy = "dynamic")
     def __repr__(self):
         return f'User {self.username}'
 
