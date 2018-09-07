6450a2ee9ed03cd447e0edf2717b9c5fa39cac4f
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-09-07 14:59:16 +0300

    created errors module to check for a foutOwfour template. i added content in my forms to create a user login and registration form

--- app/models.py
+++ app/models.py
@@ -1,14 +1,16 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
+from . import login_manager
 #creating database for class user
 
 class User(|UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
     pass_secure  = db.Column(db.String(255))
-
+    password_hash = db.Column(db.String(255))
         @property
         def password(self):
             raise AttributeError('You cannot read the password attribute')
