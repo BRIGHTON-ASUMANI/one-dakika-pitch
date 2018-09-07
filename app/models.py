22d3e6bb1976a3664c21a650a2e8905b102043e6
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-09-07 13:00:17 +0300

    started using migration sfter initializing it. TThis created a migration folder in our root folder

--- app/models.py
+++ app/models.py
@@ -6,6 +6,16 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
-
+    comment = db.relationship("Comment", backref="user", lazy = "dynamic")
     def __repr__(self):
         return f'User {self.username}'
+
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.name}'
--- manage.py
+++ manage.py
@@ -1,13 +1,13 @@
 from app import create_app, db
 from flask_script import Manager, Server
-from app.models import User
+from app.models import User,Comment
 from flask_migrate import Migrate, MigrateCommand
 
 # create apdevelopment
 app =  create_app('development')
 
 
-
+#initializing migrate command and migrate class
 manager = Manager(app)
 manager.add_command('server', Server)
 
@@ -20,10 +20,11 @@ manager.add_command('server', Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db, User = User)
+    return dict(app = app, db = db, User = User, Comment = Comment)
 
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
+    app.secret_key = 'emojis'
     manager.run()
