75d68991f5753f45ce0c9128d3bf2666d57dc41f
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-09-08 19:56:46 +0300

    i completed working on the profile template. one could be able to update their profile

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,4 +1,6 @@
-
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField
+from wtforms.validators import Required
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
