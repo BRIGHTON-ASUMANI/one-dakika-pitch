c2c0aa4dbc12bfd35ea9f25cf3bb26b398dfe98e
Brighton <36225890+BRIGHTON-ASUMANI@users.noreply.github.com>
2021-07-01 17:55:19 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,1 @@
-export MAIL_USERNAME='asumanibrighton@gmail.com'
-export MAIL_PASSWORD='newa$um0ney'
-export DATABASE_URL='postgres://evvvfvjydjbqvw:239581d2a0f92fd3f1c8d4992f1af3dc06863762c3d0f6b0aa1ddf8d4ad70d0a@ec2-174-129-32-37.compute-1.amazonaws.com:5432/dj6i9u8g7rhkk'
-python3.6 manage.py server
+
